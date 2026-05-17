4d6fa56db473b0c40e914065f367ddfc82c81265
Zeynep Doğan <zseher66@gmail.com>
2026-05-17 19:43:26 +0300

    dwt şablonu oluşturuldu

--- script/script.js
+++ script/script.js
@@ -95,3 +95,52 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
 });
+
+/* ==========================================================================
+   DREAMWEAVER DAVRANIŞLARI (GRADIN & COMPATIBILITY)
+   ========================================================================== */
+
+function MM_preloadImages() { //v3.0
+    var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+        var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+        if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_swapImgRestore() { //v3.0
+    var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_findObj(n, d) { //v4.01
+    var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+        d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+    if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+    for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+    if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+    var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+    if ((x=MM_findObj(a[i]))!=null){
+        document.MM_sr[j++]=x;
+        if(!x.oSrc) x.oSrc=x.src;
+        
+        // Modern geçiş efekti entegrasyonu (opacity animasyonu ile)
+        x.style.opacity = '0';
+        (function(element, newSrc) {
+            setTimeout(function() {
+                element.src = newSrc;
+                element.style.opacity = '1';
+            }, 180);
+        })(x, a[i+2]);
+        
+        // Küçük resimlerde seçili class'ını güncelle
+        var thumbs = document.querySelectorAll('.galeri_kutu img');
+        thumbs.forEach(function(img) {
+            if (img.src.indexOf(a[i+2]) !== -1 || a[i+2].indexOf(img.getAttribute('src')) !== -1) {
+                img.classList.add('secili');
+            } else {
+                img.classList.remove('secili');
+            }
+        });
+    }
+}
